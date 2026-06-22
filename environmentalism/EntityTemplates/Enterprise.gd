1329050dbbb5c38d60456a856c7c6326fd3a0f62
AlexeyRenzyak <157267553+AlexeyRenzyak@users.noreply.github.com>
2026-06-22 14:09:33 +0500

    Initial Project Files Commit
    Done UI templates, initial global object templates (World, Enterprise classes), added Godot Object Serializer plugin, included Licenses file

---
+++ environmentalism/EntityTemplates/Enterprise.gd
@@ -0,0 +1,14 @@
+extends Node
+class_name Enterprise
+
+var company_name : String
+
+var funds : float
+var manpower : int
+var environmental : int
+var social : int
+var governance : int
+
+var buildings : Array
+var construction_queue : Array
+var policies : Array
---
+++ environmentalism/EntityTemplates/World.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var current_enterprise
+
+var ecosystem : int = 10000
+
+var events_pool : Array
